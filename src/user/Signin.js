ffde860f224345515aaa02cff33e2c2157aad66d
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-12-11 11:22:28 +0530

    Initial setup of react App completed with proper layout and routing setup

---
+++ src/user/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../core/Layout'
+
+const Signin = ()=>(
+    <Layout title="Signin" description="Sign in to Ecommerce Node React App" >
+        ...
+    </Layout>
+)
+
+
+export default Signin;
+
+
---
+++ src/user/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../core/Layout'
+import {API} from '../config'
+
+const Signup = ()=>(
+    <Layout title="Signup" description="Signup to Ecommerce Node React App" >
+        {API}
+    </Layout>
+)
+
+
+export default Signup;
+
+
