d5945cbfdebf5f2475474a40f315b08fdfa73f17
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-12-13 12:35:08 +0530

    Creation of product by the admin is completed

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,6 +1,5 @@
 import {API} from '../config'
 
-
 export const createCategory = (userId, token, category)=>{
     // console.log(name, email, password);
     return fetch(`${API}/category/create/${userId}`, {
@@ -16,6 +15,35 @@ export const createCategory = (userId, token, category)=>{
         return response.json()
     })
     .catch(err=>{
+        console.log(err);
+    })
+}
+export const createProduct = (userId, token, product)=>{
+    // console.log(name, email, password);
+    return fetch(`${API}/product/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json', 
+            Authorization: `Bearer ${token}`
+        }, 
+        body: product
+    })
+    .then(response=>{
+        return response.json()
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+}
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    })
+    .then(response=>{
+        return response.json()
+    })
+    .catch(err => {
         console.log(err);
     })
 }
