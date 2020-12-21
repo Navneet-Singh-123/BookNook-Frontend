191388276e000e4304fee3a2577a2a39f56b90b3
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-12-21 16:00:55 +0530

    Completed Credit card functionality for paying

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import Layout from './Layout'
-import {getProducts, getBraintreeClientToken} from './apiCore'
+import {getProducts, getBraintreeClientToken, processPayment} from './apiCore'
 import Card from './Card'
 import Search from './Search';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
 import DropIn from 'braintree-web-drop-in-react';
+import {emptyCart} from './cartHelpers'
 
 
 const Checkout = ({ products, setRun = f => f, run = undefined }) => { 
@@ -58,8 +59,75 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
         )
     }
 
+    const buy = () => {
+        setData({ loading: true });
+        // send the nonce to your server
+        // nonce = data.instance.requestPaymentMethod()
+        let nonce;
+        let getNonce = data.instance.requestPaymentMethod()
+            .then(data => {
+                // console.log(data);
+                nonce = data.nonce; 
+                // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+                // and also total to be charged
+                // console.log(
+                //     "send nonce and total to process: ",
+                //     nonce,
+                //     getTotal(products)
+                // );
+                const paymentData = {
+                    paymentMethodNonce: nonce,
+                    amount: getTotal(products)
+                };
+
+                processPayment(userId, token, paymentData)
+                    .then(response => {
+                        console.log(response);
+                        setData({ ...data, success: response.success });
+
+                        // empty cart
+                        emptyCart(() => {
+                            console.log("Payment Success and cart is emptied");
+                        })
+                        // create order
+
+                        // const createOrderData = {
+                        //     products: products,
+                        //     transaction_id: response.transaction.id,
+                        //     amount: response.transaction.amount,
+                        //     address: deliveryAddress
+                        // };
+
+                        // createOrder(userId, token, createOrderData)
+                        //     .then(response => {
+                        //         emptyCart(() => {
+                        //             setRun(!run); // run useEffect in parent Cart
+                        //             console.log('payment success and empty cart');
+                        //             setData({
+                        //                 loading: false,
+                        //                 success: true
+                        //             });
+                        //         });
+                        //     })
+                        //     .catch(error => {
+                        //         console.log(error);
+                        //         setData({ loading: false });
+                        //     });
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        setData({ loading: false });
+                    });
+            })
+            .catch(error => {
+                // console.log("dropin error: ", error);
+                setData({ ...data, error: error.message });
+            });
+    };
+
+
     const showDropIn = () => (
-        <div>
+        <div onBlur={() => setData({ ...data, error: '' })}>
             {data.clientToken !== null && products.length > 0 ? (
                 <div>
                     <DropIn 
@@ -68,15 +136,33 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
                         }}
                         onInstance={instance => (data.instance = instance)}
                     />
-                    <button className="btn btn-success">Checkout</button>
+                    <button onClick={buy} className="btn btn-success btn-block">Pay</button>
                 </div>
             ) : null}
         </div>
     )
 
+    const showError = error => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = success => (
+        <div className="alert alert-info" style={{ display: success ? '' : 'none' }}>
+            Thanks! Your payment was successful!
+        </div>
+    );
+
+
+    const showLoading = loading => loading && <h2 className="text-success">Loading...</h2>;
+
     return (
         <div>
             <h2>Total: Rs {getTotal()} </h2>
+            {showLoading(data.loading)}
+            {showSuccess(data.success)}
+            {showError(data.error)}
             {showCheckout()}
         </div>
     )
